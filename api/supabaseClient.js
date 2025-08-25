2458143d9e8ef4cdd2e13144dcedae73b370a31b
schormeikerlugo <schormeikerl@gmail.com>
2025-08-24 23:40:17 -0400

    Se ha realiado el cambio de estructura de directorios para una mejor escalabilidad del proyecto

--- api/supabaseClient.js
+++ api/supabaseClient.js
@@ -1,7 +1,7 @@
 // public/js/supabaseClient.js
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.39.7/+esm';
+import { createClient } from 'https://esm.sh/@supabase/supabase-js';
 
 const SUPABASE_URL = 'https://wvebiyuoszwzsxavoitp.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind2ZWJpeXVvc3p3enN4YXZvaXRwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAxMDAzMjksImV4cCI6MjA2NTY3NjMyOX0.l7SNxCXPfRsZ4uyTAmqpA00veBpZDbDAcy4oBBll5QI';
+export const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind2ZWJpeXVvc3p3enN4YXZvaXRwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAxMDAzMjksImV4cCI6MjA2NTY3NjMyOX0.l7SNxCXPfRsZ4uyTAmqpA00veBpZDbDAcy4oBBll5QI';
 
 export const client = createClient(SUPABASE_URL, SUPABASE_KEY);
